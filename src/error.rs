ac5cd6d40ac2f2894f12c33921ab133d04a9a7b9
Steven Roose <steven@stevenroose.org>
2018-09-25 12:54:19 +0100

    Implement macros for calls and optional arguments

---
+++ src/error.rs
@@ -0,0 +1,71 @@
+use std::error;
+use std::fmt;
+
+use bitcoin;
+use hex;
+use jsonrpc;
+use serde_json;
+
+/// The error type for errors produced in this library.
+#[derive(Debug)]
+pub enum Error {
+	JsonRpc(jsonrpc::error::Error),
+	FromHex(hex::FromHexError),
+	Json(serde_json::error::Error),
+	BitcoinSerialization(bitcoin::network::serialize::Error),
+}
+
+impl From<jsonrpc::error::Error> for Error {
+	fn from(e: jsonrpc::error::Error) -> Error {
+		Error::JsonRpc(e)
+	}
+}
+
+impl From<hex::FromHexError> for Error {
+	fn from(e: hex::FromHexError) -> Error {
+		Error::FromHex(e)
+	}
+}
+
+impl From<serde_json::error::Error> for Error {
+	fn from(e: serde_json::error::Error) -> Error {
+		Error::Json(e)
+	}
+}
+
+impl From<bitcoin::network::serialize::Error> for Error {
+	fn from(e: bitcoin::network::serialize::Error) -> Error {
+		Error::BitcoinSerialization(e)
+	}
+}
+
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			Error::JsonRpc(ref e) => write!(f, "JSON-RPC error: {}", e),
+			Error::FromHex(ref e) => write!(f, "hex decode error: {}", e),
+			Error::Json(ref e) => write!(f, "JSON error: {}", e),
+			Error::BitcoinSerialization(ref e) => write!(f, "Bitcoin serialization error: {}", e),
+		}
+	}
+}
+
+impl error::Error for Error {
+	fn description(&self) -> &str {
+		match *self {
+			Error::JsonRpc(_) => "JSON-RPC error",
+			Error::FromHex(_) => "hex decode error",
+			Error::Json(_) => "JSON error",
+			Error::BitcoinSerialization(_) => "Bitcoin serialization error",
+		}
+	}
+
+	fn cause(&self) -> Option<&error::Error> {
+		match *self {
+			Error::JsonRpc(ref e) => Some(e),
+			Error::FromHex(ref e) => Some(e),
+			Error::Json(ref e) => Some(e),
+			Error::BitcoinSerialization(ref e) => Some(e),
+		}
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,10 @@ extern crate jsonrpc;
 extern crate num_bigint;
 extern crate serde;
 extern crate serde_json;
-extern crate strason;
 
 use std::collections::HashMap;
-use std::error;
-use std::fmt;
 
-use bitcoin::blockdata::block::Block;
+use bitcoin::blockdata::block::{Block, BlockHeader};
 use bitcoin::blockdata::script::Script;
 use bitcoin::blockdata::transaction::{Transaction, SigHashType};
 use bitcoin::network::encodable::ConsensusDecodable;
@@ -34,69 +31,10 @@ use num_bigint::BigUint;
 use serde::de::Error as SerdeError;
 use serde::Deserialize;
 
-/// The error type for errors produced in this library.
-#[derive(Debug)]
-pub enum Error {
-	JsonRpc(jsonrpc::error::Error),
-	FromHex(hex::FromHexError),
-	Strason(strason::Error),
-	BitcoinSerialization(bitcoin::network::serialize::Error),
-}
+mod error;
 
-impl From<jsonrpc::error::Error> for Error {
-	fn from(e: jsonrpc::error::Error) -> Error {
-		Error::JsonRpc(e)
-	}
-}
+pub use error::Error;
 
-impl From<hex::FromHexError> for Error {
-	fn from(e: hex::FromHexError) -> Error {
-		Error::FromHex(e)
-	}
-}
-
-impl From<strason::Error> for Error {
-	fn from(e: strason::Error) -> Error {
-		Error::Strason(e)
-	}
-}
-
-impl From<bitcoin::network::serialize::Error> for Error {
-	fn from(e: bitcoin::network::serialize::Error) -> Error {
-		Error::BitcoinSerialization(e)
-	}
-}
-
-impl fmt::Display for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match *self {
-			Error::JsonRpc(ref e) => write!(f, "JSON-RPC error: {}", e),
-			Error::FromHex(ref e) => write!(f, "hex decode error: {}", e),
-			Error::Strason(ref e) => write!(f, "JSON error: {}", e),
-			Error::BitcoinSerialization(ref e) => write!(f, "Bitcoin serialization error: {}", e),
-		}
-	}
-}
-
-impl error::Error for Error {
-	fn description(&self) -> &str {
-		match *self {
-			Error::JsonRpc(_) => "JSON-RPC error",
-			Error::FromHex(_) => "hex decode error",
-			Error::Strason(_) => "JSON error",
-			Error::BitcoinSerialization(_) => "Bitcoin serialization error",
-		}
-	}
-
-	fn cause(&self) -> Option<&error::Error> {
-		match *self {
-			Error::JsonRpc(ref e) => Some(e),
-			Error::FromHex(ref e) => Some(e),
-			Error::Strason(ref e) => Some(e),
-			Error::BitcoinSerialization(ref e) => Some(e),
-		}
-	}
-}
 
 #[derive(Deserialize, Clone, PartialEq, Eq, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -175,6 +113,7 @@ pub struct GetRawTransactionResultVoutScriptPubKey {
 #[derive(Deserialize, Clone, PartialEq, Eq, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct GetRawTransactionResultVout {
+	#[serde(deserialize_with = "deserialize_amount")]
 	pub value: Amount,
 	pub n: u32,
 	pub script_pub_key: GetRawTransactionResultVoutScriptPubKey,
@@ -184,7 +123,7 @@ pub struct GetRawTransactionResultVout {
 #[serde(rename_all = "camelCase")]
 pub struct GetRawTransactionResult {
 	#[serde(rename = "in_active_chain")]
-	pub in_active_chain: bool,
+	pub in_active_chain: Option<bool>,
 	#[serde(deserialize_with = "deserialize_hex")]
 	pub hex: Vec<u8>,
 	pub txid: Sha256dHash,
@@ -194,7 +133,7 @@ pub struct GetRawTransactionResult {
 	pub version: u32,
 	pub locktime: u32,
 	pub vin: Vec<GetRawTransactionResultVin>,
-	pub vout: Vec<GetRawTransactionResultVoutScriptPubKey>,
+	pub vout: Vec<GetRawTransactionResultVout>,
 	pub blockhash: Sha256dHash,
 	pub confirmations: usize,
 	pub time: usize,
@@ -276,6 +215,84 @@ pub struct Client {
 	client: jsonrpc::client::Client,
 }
 
+enum Arg {
+	Required(serde_json::Value),
+	OptionalSet(serde_json::Value),
+	OptionalDefault(serde_json::Value),
+}
+
+macro_rules! arg {
+	($val:expr) => {
+		Arg::Required(serde_json::to_value($val)?)
+	};
+	($val:expr, $def:expr) => {
+		match $val {
+			Some(v) => Arg::OptionalSet(serde_json::to_value(v)?),
+			None => Arg::OptionalDefault(serde_json::to_value($def)?),
+		}
+	};
+	($val:expr,) => { arg!($val, "") };
+}
+
+macro_rules! empty {
+	() => { { let v: Vec<serde_json::Value> = vec![]; v } }
+}
+
+macro_rules! make_call {
+	($self:ident, $method:expr) => { make_call!($self, $method,) };
+	($self:ident, $method:expr, $($arg:expr),*) => {
+		{
+			// We want to truncate the argument to remove the trailing non-set optional arguments.
+			// This makes sure we don't send default values if we don't really need to and this 
+			// can prevent unexpected behaviour if the server changes its default values.
+			let mut args = Vec::new();
+			$( args.push($arg); )*
+			while let Some(Arg::OptionalDefault(_)) = args.last() {
+				args.pop();
+			}
+			let json_args = args.into_iter().map(|a| match a {
+				Arg::Required(v) => v,
+				Arg::OptionalSet(v) => v,
+				Arg::OptionalDefault(v) => v,
+			}).collect();
+			let req = $self.client.build_request($method.to_string(), json_args);
+			$self.client.send_request(&req).map_err(Error::from)
+		}
+	}
+}
+
+/// Convert a response object into the provided type.
+macro_rules! result_json {
+	($resp:ident, $json_type:ty) => {
+		$resp.and_then(|r| r.into_result::<$json_type>().map_err(Error::from))
+	}
+}
+
+macro_rules! result_raw {
+	($resp:ident, Option<$raw_type:ty>) => {
+		{
+			let hex_opt = $resp.and_then(|r| r.into_result::<Option<String>>()
+					.map_err(Error::from))?;
+			match hex_opt {
+				Some(hex) => {
+					let raw = hex::decode(hex)?;
+					match <$raw_type>::consensus_decode(&mut RawDecoder::new(raw.as_slice())) {
+						Ok(val) => Ok(Some(val)),
+						Err(e) => Err(e.into()),
+					}
+				},
+				None => Ok(None),
+			}
+		}
+	};
+	($resp:ident, $raw_type:ty) => {
+		$resp.and_then(|r| r.into_result::<String>().map_err(Error::from))
+			 .and_then(|h| hex::decode(h).map_err(Error::from))
+			 .and_then(|r| <$raw_type>::consensus_decode(&mut RawDecoder::new(r.as_slice()))
+					.map_err(Error::from))
+	};
+}
+
 impl Client {
 	pub fn new(uri: String, user: Option<String>, pass: Option<String>) -> Client {
 		Client {
@@ -286,58 +303,36 @@ impl Client {
 	// Methods have identical casing to API methods on purpose.
 	// Variants of API methods are formed using an underscore.
 
-	pub fn getblock_raw(&mut self, hash: Sha256dHash) -> Result<bitcoin::Block, Error> {
-		let args: Vec<strason::Json> = vec![hash.to_string().into(), 0.into()];
-		let req = self.client.build_request("getblock".to_string(), args);
-		self.client
-			.send_request(&req)
-			.map_err(Error::from)
-			.and_then(|res| res.into_result::<String>().map_err(Error::from))
-			.and_then(|raw| hex::decode(raw).map_err(Error::from))
-			.and_then(|byt| {
-				Block::consensus_decode(&mut RawDecoder::new(&byt[..])).map_err(Error::from)
-			})
+	pub fn getblock_raw(&mut self, hash: Sha256dHash) -> Result<Block, Error> {
+		let resp = make_call!(self, "getblock", arg!(hash), arg!(0));
+		result_raw!(resp, Block)
 	}
 
 	pub fn getblock_info(&mut self, hash: Sha256dHash) -> Result<GetBlockResult, Error> {
-		let args: Vec<strason::Json> = vec![hash.to_string().into(), 1.into()];
-		let req = self.client.build_request("getblock".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<GetBlockResult>())
-			.map_err(Error::from)
+		let resp = make_call!(self, "getblock", arg!(hash), arg!(1));
+		result_json!(resp, GetBlockResult)
 	}
 	//TODO(stevenroose) getblock_raw (should be serialized to
 	// bitcoin::blockdata::Block) and getblock_txs
 
 	pub fn getblockcount(&mut self) -> Result<usize, Error> {
-		let req = self
-			.client
-			.build_request("getblockclount".to_string(), vec![]);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<usize>())
-			.map_err(Error::from)
+		let resp = make_call!(self, "getblockcount");
+		result_json!(resp, usize)
 	}
 
 	pub fn getblockhash(&mut self, height: u32) -> Result<Sha256dHash, Error> {
-		let args: Vec<strason::Json> = vec![height.into()];
-		let req = self.client.build_request("getblockhash".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<Sha256dHash>())
-			.map_err(Error::from)
+		let resp = make_call!(self, "getblockhash", arg!(height));
+		result_json!(resp, Sha256dHash)
 	}
 
-	pub fn getblockheader(&mut self, hash: Sha256dHash) -> Result<GetBlockHeaderResult, Error> {
-		let args: Vec<strason::Json> = vec![hash.to_string().into(), 1.into()];
-		let req = self
-			.client
-			.build_request("getblockheader".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<GetBlockHeaderResult>())
-			.map_err(Error::from)
+	pub fn getblockheader(&mut self, hash: Sha256dHash) -> Result<BlockHeader, Error> {
+		let resp = make_call!(self, "getblockheader", arg!(hash), arg!(true));
+		result_raw!(resp, BlockHeader)
+	}
+
+	pub fn getblockheader_verbose(&mut self, hash: Sha256dHash) -> Result<GetBlockHeaderResult, Error> {
+		let resp = make_call!(self, "getblockheader", arg!(hash), arg!(true));
+		result_json!(resp, GetBlockHeaderResult)
 	}
 
 	pub fn getrawtransaction(
@@ -345,24 +340,8 @@ impl Client {
 		txid: Sha256dHash,
 		block_hash: Option<Sha256dHash>,
 	) -> Result<Option<Transaction>, Error> {
-		let mut args: Vec<strason::Json> = vec![txid.to_string().into(), false.into()];
-		if let Some(hash) = block_hash {
-			args.push(hash.to_string().into());
-		}
-
-		let req = self.client.build_request("getrawtransaction".to_string(), args);
-		let raw_opt = self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<Option<Vec<u8>>>())
-			.map_err(Error::from)?;
-		if let Some(raw) = raw_opt {
-			match Transaction::consensus_decode(&mut RawDecoder::new(raw.as_slice())) {
-				Ok(tx) => Ok(Some(tx)),
-				Err(e) => Err(e.into()),
-			}
-		} else {
-			Ok(None)
-		}
+		let resp = make_call!(self, "getrawtransaction", arg!(txid), arg!(false), arg!(block_hash));
+		result_raw!(resp, Option<Transaction>)
 	}
 
 	pub fn getrawtransaction_verbose(
@@ -370,16 +349,8 @@ impl Client {
 		txid: Sha256dHash,
 		block_hash: Option<Sha256dHash>,
 	) -> Result<Option<GetRawTransactionResult>, Error> {
-		let mut args: Vec<strason::Json> = vec![txid.to_string().into(), true.into()];
-		if let Some(hash) = block_hash {
-			args.push(hash.to_string().into());
-		}
-
-		let req = self.client.build_request("getrawtransaction".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<Option<GetRawTransactionResult>>())
-			.map_err(Error::from)
+		let resp = make_call!(self, "getrawtransaction", arg!(txid), arg!(true), arg!(block_hash));
+		result_json!(resp, Option<GetRawTransactionResult>)
 	}
 
 	pub fn gettxout(
@@ -388,17 +359,8 @@ impl Client {
 		vout: u32,
 		include_mempool: Option<bool>,
 	) -> Result<Option<GetTxOutResult>, Error> {
-		let mut args: Vec<strason::Json> = vec![txid.to_string().into(), vout.into()];
-		if let Some(b) = include_mempool {
-			args.push(b.into());
-		}
-
-		let req = self.client.build_request("gettxout".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<Option<GetTxOutResult>>())
-			.map_err(Error::from)
-			.into()
+		let resp = make_call!(self, "gettxout", arg!(txid), arg!(vout), arg!(include_mempool,));
+		result_json!(resp, Option<GetTxOutResult>)
 	}
 
 	pub fn listunspent(
@@ -409,52 +371,9 @@ impl Client {
 		include_unsafe: Option<bool>,
 		query_options: Option<HashMap<String, String>>,
 	) -> Result<Vec<ListUnspentResult>, Error> {
-		// Only provide the minimum required arguments. Provide defaults if
-		// later arguments are provided.
-		let mut args: Vec<strason::Json> = Vec::new();
-		if let Some(min) = minconf {
-			args.push(min.into());
-		} else if maxconf != None
-			|| addresses != None
-			|| include_unsafe != None
-			|| query_options != None
-		{
-			args.push(0.into());
-		}
-
-		if let Some(max) = maxconf {
-			args.push(max.into());
-		} else if addresses != None || include_unsafe != None || query_options != None {
-			args.push(9999999.into());
-		}
-
-		if let Some(addr) = addresses {
-			let mut addrs: Vec<strason::Json> = vec![];
-			for a in addr.iter() {
-				addrs.push(a.to_string().into());
-			}
-			args.push(addrs.into());
-		} else if include_unsafe != None || query_options != None {
-			let v: Vec<strason::Json> = vec![];
-			args.push(v.into());
-		}
-
-		if let Some(inc) = include_unsafe {
-			args.push(inc.into());
-		} else if query_options != None {
-			args.push(true.into());
-		}
-
-		if let Some(opts) = query_options {
-			args.push(strason::Json::from_serialize(opts)?);
-		}
-
-		let req = self.client.build_request("listunspent".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<Vec<ListUnspentResult>>())
-			.map_err(Error::from)
-			.into()
+		let resp = make_call!(self, "listunspent", arg!(minconf, 0), arg!(maxconf, 9999999),
+			arg!(addresses, empty!()), arg!(include_unsafe, true), arg!(query_options,));
+		result_json!(resp, Vec<ListUnspentResult>)
 	}
 
 	pub fn signrawtransaction(
@@ -464,51 +383,27 @@ impl Client {
 		private_keys: Option<Vec<Vec<u8>>>,
 		sighash_type: Option<SigHashType>,
 	) -> Result<SignRawTransactionResult, Error> {
-		let mut args: Vec<strason::Json> = Vec::new();
-		args.push(hex::encode(tx).into());
-
-		if let Some(utxos_val) = utxos {
-			let mut utxos_json: Vec<strason::Json> = vec![];
-			for u in utxos_val.iter() {
-				utxos_json.push(strason::Json::from_serialize(u)?);
-			}
-			args.push(utxos_json.into());
-		} else if private_keys != None || sighash_type != None {
-			args.push("".into())
-		}
-
-		if let Some(privkeys) = private_keys {
-			//TODO(stevenroose) encode private keys to WIF
-			args.push("".into())
-		} else if sighash_type != None {
-			args.push("".into())
-		}
-
-		if let Some(sighash) = sighash_type {
-			args.push(sighash_string(sighash).into());
-		}
-
-		let req = self
-			.client
-			.build_request("signrawtransaction".to_string(), args);
-		self.client
-			.send_request(&req)
-			.and_then(|res| res.into_result::<SignRawTransactionResult>())
-			.map_err(Error::from)
-			.into()
+		let sighash = sighash_string(sighash_type);
+		let resp = make_call!(self, "signrawtransaction", arg!(hex::encode(tx)),
+			arg!(utxos, empty!()), arg!(Some(empty!()), empty!()),//TODO(stevenroose) impl privkeys
+			arg!(sighash,));
+		result_json!(resp, SignRawTransactionResult)
 	}
 }
 
 /// Convert a SigHashType object to a string representation used in the API.
-fn sighash_string(sighash: SigHashType) -> String {
-	String::from(match sighash {
-		SigHashType::All => "ALL",
-		SigHashType::None => "NONE",
-		SigHashType::Single => "SINGLE",
-		SigHashType::AllPlusAnyoneCanPay => "ALL|ANYONECANPAY",
-		SigHashType::NonePlusAnyoneCanPay => "NONE|ANYONECANPAY",
-		SigHashType::SinglePlusAnyoneCanPay => "SINGLE|ANYONECANPAY",
-	})
+fn sighash_string(sighash: Option<SigHashType>) -> Option<String> {
+	match sighash {
+		None => None,
+		Some(sh) => Some(String::from(match sh {
+			SigHashType::All => "ALL",
+			SigHashType::None => "NONE",
+			SigHashType::Single => "SINGLE",
+			SigHashType::AllPlusAnyoneCanPay => "ALL|ANYONECANPAY",
+			SigHashType::NonePlusAnyoneCanPay => "NONE|ANYONECANPAY",
+			SigHashType::SinglePlusAnyoneCanPay => "SINGLE|ANYONECANPAY",
+		})),
+	}
 }
 
 /// Deserializes a BTC-denominated floating point Bitcoin amount into the Amount
