f00f6f23d0d9cfd1058bdb1f1c4656f3d5939517
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-11-07 23:01:23 -0800

    Port the `Querable` stuff from `rust-bitcoin-rpc`

--- src/client.rs
+++ src/client.rs
@@ -188,6 +188,47 @@ where
 	Ok(T::consensus_decode(&mut io::Cursor::new(bytes))?)
 }
 
+/// A type that can be used as an id when querying for `Querable`
+// TODO: Unnecessary? Always `Sha256dHash`? --dpc
+pub trait Id {
+	fn to_json_value(&self) -> serde_json::value::Value;
+}
+
+impl Id for Sha256dHash {
+	fn to_json_value(&self) -> serde_json::value::Value {
+		self.to_string().into()
+	}
+}
+
+/// A type that can be queried from the Node
+pub trait Querable: Sized {
+	/// Type of the id used to query the item
+	type Id: Id;
+	/// Query the item using `rpc` and convert to `Self`
+	fn query(rpc: &mut Client, id: &Self::Id) -> Result<Self>;
+}
+
+impl Querable for bitcoin::blockdata::block::Block {
+	type Id = Sha256dHash;
+
+	fn query(rpc: &mut Client, id: &Self::Id) -> Result<Self> {
+		let rpc_name = "getblock";
+		let hex: String = rpc.call(rpc_name, &[id.to_json_value(), 0.into()])?;
+		let bytes = bitcoin::util::misc::hex_bytes(&hex)?;
+		Ok(bitcoin::consensus::encode::deserialize(&bytes)?)
+	}
+}
+
+impl Querable for bitcoin::blockdata::transaction::Transaction {
+	type Id = Sha256dHash;
+
+	fn query(rpc: &mut Client, id: &Self::Id) -> Result<Self> {
+		let rpc_name = "getrawtransaction";
+		let hex: String = rpc.call(rpc_name, &[id.to_json_value()])?;
+		let bytes = bitcoin::util::misc::hex_bytes(&hex)?;
+		Ok(bitcoin::consensus::encode::deserialize(&bytes)?)
+	}
+}
 /// Shorthand for converting a variable into a serde_json::Value.
 fn into_json<T>(val: T) -> Result<serde_json::Value>
 where
@@ -314,6 +355,11 @@ impl Client {
 		Client {
 			client: client,
 		}
+	}
+
+	/// Query an object implementing `Querable` type
+	pub fn get_by_id<T: Querable>(&mut self, id: &<T as Querable>::Id) -> Result<T> {
+		T::query(self, &id)
 	}
 
 	fn call<T: for<'a> serde::de::Deserialize<'a>>(
