44ecb408a2de07c42f2db1f9fa6c768ecfe2e5bc
Steven Roose <steven@stevenroose.org>
2019-04-11 13:03:18 +0100

    Add fundrawtransaction command

--- client/src/client.rs
+++ client/src/client.rs
@@ -388,6 +388,17 @@ pub trait RpcApi: Sized {
         Ok(bitcoin::consensus::encode::deserialize(&bytes)?)
     }
 
+    fn fund_raw_transaction<R: RawTx>(
+        &self,
+        tx: R,
+        options: Option<json::FundRawTransactionOptions>,
+        is_witness: Option<bool>,
+    ) -> Result<json::FundRawTransactionResult> {
+        let mut args = [tx.raw_hex().into(), opt_into_json(options)?, opt_into_json(is_witness)?];
+        let defaults = [empty_obj(), null()];
+        self.call("fundrawtransaction", handle_defaults(&mut args, &defaults))
+    }
+
     fn sign_raw_transaction<R: RawTx>(
         &self,
         tx: R,
