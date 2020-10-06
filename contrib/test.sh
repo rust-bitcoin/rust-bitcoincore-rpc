42e26c2ffaac74dec609e6c96edd7cc8c1d5051b
Steven Roose <steven@stevenroose.org>
2020-10-06 22:34:19 +0200

    Use less Travis jobs
    No longer test the entire matrix of Rust and bitcoind versions.

--- contrib/test.sh
+++ contrib/test.sh
@@ -1,4 +1,13 @@
 
+set -xe
+
+# Just echo all the relevant env vars to help debug Travis.
+echo "TRAVIS_RUST_VERSION: \"$TRAVIS_RUST_VERSION\""
+echo "RUSTFMTCHECK: \"$RUSTFMTCHECK\""
+echo "BITCOINVERSION: \"$BITCOINVERSION\""
+echo "PATH: \"$PATH\""
+
+
 # Pin dependencies for Rust v1.29
 if [ "$TRAVIS_RUST_VERSION" = "1.29.0" ]; then
     cargo generate-lockfile --verbose
@@ -10,18 +19,22 @@ if [ "$TRAVIS_RUST_VERSION" = "1.29.0" ]; then
     cargo update --verbose --package "serde_derive" --precise "1.0.98"
 fi
 
-if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
+if [ -n "$RUSTFMTCHECK" ]; then
   rustup component add rustfmt
   cargo fmt --all -- --check
 fi
 
+# Integration test.
+if [ -n "$BITCOINVERSION" ]; then
+    wget https://bitcoincore.org/bin/bitcoin-core-$BITCOINVERSION/bitcoin-$BITCOINVERSION-x86_64-linux-gnu.tar.gz
+    tar -xzvf bitcoin-$BITCOINVERSION-x86_64-linux-gnu.tar.gz
+    export PATH=$PATH:$(pwd)/bitcoin-$BITCOINVERSION/bin
+    cd integration_test
+    ./run.sh
+    exit 0
+fi
+
+# Regular build/unit test.
 cargo build --verbose
 cargo test --verbose
 cargo build --verbose --examples
-
-# Integration test
-wget https://bitcoincore.org/bin/bitcoin-core-$BITCOINVERSION/bitcoin-$BITCOINVERSION-x86_64-linux-gnu.tar.gz
-tar -xzvf bitcoin-$BITCOINVERSION-x86_64-linux-gnu.tar.gz
-export PATH=$PATH:$(pwd)/bitcoin-$BITCOINVERSION/bin
-(cd integration_test && ./run.sh)
-
