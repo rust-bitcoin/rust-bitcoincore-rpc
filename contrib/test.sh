34bde5ec8c2d784347bcb097dacf6e958636e1a3
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-05-18 15:11:12 -0600

    updates to newer jsonrpc dependencies containing swappable transports

--- contrib/test.sh
+++ contrib/test.sh
@@ -12,18 +12,13 @@ echo "PATH: \"$PATH\""
 if [ "$TRAVIS_RUST_VERSION" = "1.29.0" ]; then
     cargo generate-lockfile --verbose
 
-    # hyper depends on log 0.3 while we depnd on 0.4, so cargo doesn't know which one to pin
-    LOG_4_PATCH="$(cargo update --package "log" --precise "0.4.13" 2>&1 | sed -n "s/.*log:0.4.\([0-9]*\)/\1/p")"
-    cargo update --package "log:0.4.$LOG_4_PATCH" --precise "0.4.13"
-
+    cargo update --verbose --package "log" --precise "0.4.13"
     cargo update --verbose --package "cc" --precise "1.0.41"
     cargo update --verbose --package "cfg-if" --precise "0.1.9"
-    cargo update --verbose --package "unicode-normalization" --precise "0.1.9"
     cargo update --verbose --package "serde_json" --precise "1.0.39"
     cargo update --verbose --package "serde" --precise "1.0.98"
     cargo update --verbose --package "serde_derive" --precise "1.0.98"
     cargo update --verbose --package "byteorder" --precise "1.3.4"
-    cargo update --verbose --package "unicode-bidi" --precise "0.3.4"
 fi
 
 if [ -n "$RUSTFMTCHECK" ]; then
