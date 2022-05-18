4e888de241b29dd66a30d9802a4f2dcf57f2d22b
Tobin C. Harding <me@tobin.cc>
2022-05-18 14:51:55 +1000

    Remove 1.29 restriction from CI and README
    The rust-bitcoin stack is moving towards an MSRV of 1.41.1
    
    In preparation for updating our MSRV from 1.29 to 1.41.1 update the
    README, test script, and CI pipeline to reflect the change.

--- contrib/test.sh
+++ contrib/test.sh
@@ -6,20 +6,6 @@ echo "RUSTFMTCHECK: \"$RUSTFMTCHECK\""
 echo "BITCOINVERSION: \"$BITCOINVERSION\""
 echo "PATH: \"$PATH\""
 
-
-# Pin dependencies for Rust v1.29
-if [ -n $"$PIN_VERSIONS" ]; then
-    cargo generate-lockfile --verbose
-
-    cargo update --verbose --package "log" --precise "0.4.13"
-    cargo update --verbose --package "cc" --precise "1.0.41"
-    cargo update --verbose --package "cfg-if" --precise "0.1.9"
-    cargo update --verbose --package "serde_json" --precise "1.0.39"
-    cargo update --verbose --package "serde" --precise "1.0.98"
-    cargo update --verbose --package "serde_derive" --precise "1.0.98"
-    cargo update --verbose --package "byteorder" --precise "1.3.4"
-fi
-
 if [ -n "$RUSTFMTCHECK" ]; then
   rustup component add rustfmt
   cargo fmt --all -- --check
