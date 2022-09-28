9a430e094aed1b826d84cd7845349b9bb59b0819
rajarshimaitra <rajarshi149@gmail.com>
2022-09-28 17:59:09 +0530

    Update integration_test
    This adds the previously added `proxy` feature into integration test for
    testing the proxy functionality.

--- integration_test/src/main.rs
+++ integration_test/src/main.rs
@@ -25,8 +25,8 @@ use bitcoin::hashes::hex::{FromHex, ToHex};
 use bitcoin::hashes::Hash;
 use bitcoin::secp256k1;
 use bitcoin::{
-    Address, Amount, PackedLockTime, Network, OutPoint, PrivateKey, Script, EcdsaSighashType, SignedAmount,
-    Sequence, Transaction, TxIn, TxOut, Txid, Witness,
+    Address, Amount, EcdsaSighashType, Network, OutPoint, PackedLockTime, PrivateKey, Script,
+    Sequence, SignedAmount, Transaction, TxIn, TxOut, Txid, Witness,
 };
 use bitcoincore_rpc::bitcoincore_rpc_json::{
     GetBlockTemplateModes, GetBlockTemplateRules, ScanTxOutRequest,
@@ -119,13 +119,22 @@ fn get_auth() -> bitcoincore_rpc::Auth {
     };
 }
 
+#[cfg(feature = "proxy")]
+fn get_proxy_url() -> String {
+    return std::env::var("PROXY_URL").expect("PROXY_URL must be set in proxy feature");
+}
+
 fn main() {
     log::set_logger(&LOGGER).map(|()| log::set_max_level(log::LevelFilter::max())).unwrap();
 
     let rpc_url = format!("{}/wallet/testwallet", get_rpc_url());
     let auth = get_auth();
 
+    #[cfg(not(feature = "proxy"))]
     let cl = Client::new(&rpc_url, auth).unwrap();
+
+    #[cfg(feature = "proxy")]
+    let cl = Client::new_with_proxy(&rpc_url, auth, &get_proxy_url(), None).unwrap();
 
     test_get_network_info(&cl);
     unsafe { VERSION = cl.version().unwrap() };
