7da5d14d3a745c8b85a267f77d910db5a9eb3b69
Daniel Connolly <daniel@dconnolly.com>
2023-12-27 15:39:16 +0000

    improve script, only download binaries if we need them

--- integration_test/test_all.sh
+++ integration_test/test_all.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
 
 set -xe
 
 # Integration test.
 if [ -n "$BSVVERSION" ]; then
-    wget -nv https://download.bitcoinsv.io/bitcoinsv/$BSVVERSION/bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz
-    tar -xzvf bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz
+    if [! -d "/bitcoin-sv-$BSVVERSION/bin"]; then
+      if [! -f "bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz"]; then
+        wget -nv https://download.bitcoinsv.io/bitcoinsv/$BSVVERSION/bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz
+      fi
+      tar -xzvf bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz
+    fi
     export PATH=$PATH:$(pwd)/bitcoin-sv-$BSVVERSION/bin
     cd integration_test
     ./run.sh
