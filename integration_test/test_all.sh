c0122f648ed2988c0ae81031eab270c19d1466fd
Daniel Connolly <daniel@dconnolly.com>
2023-12-29 17:57:28 +0000

    typos

--- integration_test/test_all.sh
+++ integration_test/test_all.sh
@@ -4,8 +4,8 @@ set -xe
 
 # Integration test.
 if [ -n "$BSVVERSION" ]; then
-    if [ ! -d "bitcoin-sv-$BSVVERSION/bin"]; then
-      if [ ! -f "bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz"]; then
+    if [ ! -d "bitcoin-sv-$BSVVERSION/bin" ]; then
+      if [ ! -f "bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz" ]; then
         wget -nv https://download.bitcoinsv.io/bitcoinsv/$BSVVERSION/bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz
       fi
       tar -xzvf bitcoin-sv-$BSVVERSION-x86_64-linux-gnu.tar.gz
