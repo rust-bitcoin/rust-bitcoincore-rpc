a0fe62786f90afb82a366b61fff84ee54bf64c94
Daniel Connolly <daniel@dconnolly.com>
2025-02-08 11:55:17 +0000

    cleanup

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,18 +1,19 @@
-//! # Rust Client for Bitcoin SV API
-//!
-//! This is a client library for the Bitcoin SV JSON-RPC API.
-//!
-//! Example usage:
-//! ```norun
-//!    let rpc_client = Client::new("http://127.0.0.1:8332", Auth::UserPass("username".to_string(), "password".to_string())).unwrap();
-//!    let best_block_hash = rpc_client.get_best_block_hash().unwrap();
-//!    println!("best block hash: {}", best_block_hash);
-//! ```
-//!
-//! For documentation on which methods are available, see the [RpcApi] trait.
 
 #![crate_name = "bitcoinsv_rpc"]
 #![crate_type = "rlib"]
+
+/// # Rust Client for Bitcoin SV API
+///
+/// This is a client library for the Bitcoin SV JSON-RPC API.
+///
+/// Example usage:
+/// ```norun
+///    let rpc_client = Client::new("http://127.0.0.1:8332", Auth::UserPass("username".to_string(), "password".to_string())).unwrap();
+///    let best_block_hash = rpc_client.get_best_block_hash().unwrap();
+///    println!("best block hash: {}", best_block_hash);
+/// ```
+///
+/// For documentation on which methods are available, see the [RpcApi] trait.
 
 #[macro_use]
 extern crate log;
